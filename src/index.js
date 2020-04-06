bd389a4eb8b0f0e3f977118b05e18b30cded8947
mecha2k <mecha2k@naver.com>
2020-04-07 08:52:23 +0900

    1st commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { ApolloProvider, useQuery } from "@apollo/react-hooks";
 const client = new ApolloClient({ uri: "https://48p1r2roz4.sse.codesandbox.io" });
 
 const EXCHANGE_RATES = gql`
-  {
+  query {
     rates(currency: "USD") {
       currency
       rate
@@ -21,7 +21,7 @@ function ExchangeRates() {
   if (loading) return <p>Loading...🚀</p>;
   if (error) return <p>Error:</p>;
 
-  return data.rates.map(({ currency, rate }) => (
+  return data["rates"].map(({ currency, rate }) => (
     <div key={currency}>
       <p>
         {currency}: {rate}
@@ -33,7 +33,7 @@ function ExchangeRates() {
 const App = () => (
   <ApolloProvider client={client}>
     <div>
-      <h6>My first Apollo application...</h6>
+      <h3>My first Apollo application...</h3>
       <ExchangeRates />
     </div>
   </ApolloProvider>
