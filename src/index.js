9d839075f5626cfaf8441bb978eddc867f661b2e
mecha2k <mecha2k@naver.com>
2020-04-07 08:41:15 +0900

    1st commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import ApolloClient from "apollo-boost";
 import { gql } from "apollo-boost";
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
 
 const client = new ApolloClient({ uri: "https://48p1r2roz4.sse.codesandbox.io" });
 
@@ -14,3 +17,37 @@ client
     `,
   })
   .then((result) => console.log(result));
+
+const EXCHANGE_RATES = gql`
+  {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+function exchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...🚀</p>;
+  if (error) return <p>Error:</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h6>My first Apollo application...</h6>
+    </div>
+  </ApolloProvider>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
