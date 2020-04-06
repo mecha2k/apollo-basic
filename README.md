5e2830696ebe26f6ec309e90e3abac753039d302
mecha2k <mecha2k@naver.com>
2020-04-07 08:22:48 +0900

    1st commit

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Apollo Client Basics
+
+With React app, npm install apollo-boost @apollo/react-hooks graphql
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const client = new ApolloClient({ uri: "https://48p1r2roz4.sse.codesandbox.io" });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
