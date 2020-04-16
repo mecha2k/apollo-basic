4da25801816c3367e081972ff7b1e937b867edba
mecha2k <mecha2k@naver.com>
2020-04-16 16:26:39 +0900

    1st commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./App.css";
+
+function FancyBorder(props) {
+  return <div className={"FancyBorder FancyBorder-" + props.color}>{props.children}</div>;
+}
+
+function WelcomeDialog() {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">Welcome</h1>
+      <p className="Dialog-message">Thank you for visiting our spacecraft!</p>
+    </FancyBorder>
+  );
+}
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h2>Building Query components 🚀</h2>
+        <WelcomeDialog />
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<WelcomeDialog />, document.getElementById("root"));
